d380c29e02bb8dc350c15bb7a19f8f98f4c4a976
NickGeo1 <60719667+NickGeo1@users.noreply.github.com>
2020-06-16 17:35:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -42,20 +42,19 @@ class BinaryTreeNode
         {
             BinaryTreeNode *p = tr;
             int maxkey = p -> data;
-            BinaryTreeNode *pp = p;
+            tr -> Maxkey = p;
             p = p -> RightChild;
         
             while(p)
             {
-                pp = p;
                 if (p -> data > maxkey)
                 {
                     maxkey = p -> data;
+                    tr -> Maxkey = p;
                 }
+                
                 p = p -> RightChild;
             }
-
-        tr -> Maxkey = pp;
         }
     }
 
@@ -85,13 +84,15 @@ int main()
                         //  Left subtree of root
     
     //Leafs 
-    BinaryTreeNode nd39 (39), nd12 (12), nd22 (22);
+    BinaryTreeNode nd27 (27), nd12 (12), nd22 (22), nd20 (20);
     
     //Rest nodes
-    BinaryTreeNode nd10 (10,0,&nd12);
-    BinaryTreeNode nd13 (13,&nd10,&nd22); 
-    BinaryTreeNode nd26 (26,&nd13,0);  
-    BinaryTreeNode nd41 (41,&nd39,&nd26);
+    BinaryTreeNode nd10 (10,&nd12,0); 
+    BinaryTreeNode nd13 (13,0,&nd22); 
+    BinaryTreeNode nd26 (26,&nd13,&nd27);
+    BinaryTreeNode nd39 (39,&nd10,&nd20);
+    BinaryTreeNode nd41 (41,&nd39,&nd26); 
+                                            
      
      
      
@@ -100,18 +101,16 @@ int main()
                         //  Right subtree of root
                         
     //Leafs
-    BinaryTreeNode nd35 (35), nd9 (9), nd52 (52), nd85 (85), nd87 (87);
+    BinaryTreeNode nd35 (35), nd9 (9), nd52 (52), nd40 (40), nd90 (90), nd87 (87);
     
     //Rest nodes
-    BinaryTreeNode nd2 (2,0,&nd9);
-    BinaryTreeNode nd40 (40,&nd35,&nd2); 
+    BinaryTreeNode nd2 (2,&nd35,&nd9);
     BinaryTreeNode nd44 (44,&nd40,0);  
-    BinaryTreeNode nd61 (61,&nd44,0);
+    BinaryTreeNode nd61 (61,&nd2,&nd44);
     
-    BinaryTreeNode nd90 (90,&nd85,&nd87); 
-    BinaryTreeNode nd101 (101,&nd90,0);  
+    BinaryTreeNode nd101 (101,&nd90,&nd87);  
     BinaryTreeNode nd59 (59,&nd52,&nd101);
-    BinaryTreeNode nd67 (67,&nd61,&nd59);
+    BinaryTreeNode nd67 (67,&nd61,&nd59); 
     
                         
                         
@@ -119,7 +118,7 @@ int main()
                         
                         // Root
      
-    BinaryTreeNode rt50 (50,&nd41,&nd67);
+    BinaryTreeNode rt50 (50,&nd41,&nd67); 
     BinaryTreeNode *root = &rt50; //Pointer to root. 
     BinaryTreeNode *tree = root;  //Now root is our tree because its the node that contains the rest nodes.
 //###################################################################################    
